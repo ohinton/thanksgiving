9a5accca99ab224b60733d6dd6fad32696fc7ce2
Olivia Hinton <hinton.olivia@gmail.com>
2016-11-22 15:46:09 -0800

    start php practice project

---
+++ helloworld.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello World!</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Shrikhand|Sue+Ellen+Francisco" rel="stylesheet">
+    <link href="styles.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+  <div class="container">
+    <h1>Happy Thanksgiving!</h1>
+    <div class="row">
+      <div class="col-md-6">
+        <img class="img-responsive img-rounded" src="thanksgiving.jpeg" alt="Thanksgiving Dinner">
+      </div>
+      <div class="col-md-6">
+        <p>Dear family,</p>
+        <p>How are you? I hope that you are having a nice weekend. I'm vacationing in the mountains of Tibet while I learn programming! </p>
+        <p>Jennifer, you would not believe how cold it is here. I should have gone to Hawaii instead. But I like programming a lot, so I've got that going for me. </p>
+        <p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>
+        <p>Cheers,</p>
+        <p>Olivia</p>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
